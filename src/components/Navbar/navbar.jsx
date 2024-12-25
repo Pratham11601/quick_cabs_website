448eee9bfb207a83b5bf01910d0b7c6c757785af
Rohan Koli <138271391+Rohan-Koli@users.noreply.github.com>
2024-12-25 17:40:22 +0530

    changes

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -3,6 +3,10 @@ import React, { useState } from "react";
 import { HiMenuAlt3, HiMenuAlt1 } from "react-icons/hi";
 import { IoIosArrowDown } from "react-icons/io";
 import { Link } from "react-router-dom";
+import image1 from "../../assets/image7.jpg";
+import image2 from "../../assets/bride.jpg";
+import image3 from "../../assets/makeup1.jpg";
+import image4 from "../../assets/image5.jpg";
 export const Navlinks = [
   {
     id: 1,
@@ -171,13 +175,276 @@ export const Navlinks = [
     id: 3,
     name: "CATEGORIES",
     subMenu: [
-      { name: "Astrology", link: "/categories/astrology" },
-      { name: "Tarot card reader", link: "/categories/tarot-card-reader" },
-      { name: "Beautician", link: "/categories/beautician" },
-      { name: "Makeup artist", link: "/categories/makeup-artist" },
-      { name: "Mehendi artist", link: "/categories/mehendi-artist" },
-      { name: "Tattoo artist", link: "/categories/tattoo-artist" },
-      { name: "Hair salons", link: "/categories/hair-salons" },
+      {
+        name: "Astrology",
+        link: "/categories/makeup-artist",
+        heroSection: {
+          text: "Discover Your Destiny with Expert Astrology",
+          img: "	https://i.pinimg.com/736x/23/28/03/232803c12ea29188f1dff35e334cf329.jpg", // Replace with actual image URL
+        },
+        section1: {
+          heading: "Unlock the Secrets of the Stars",
+          img: "https://i.pinimg.com/736x/d1/ed/ad/d1edaddd1f225755588208cbbf319f3f.jpg", // Replace with actual image URL
+          para1:
+            "Our expert astrologers provide insights into your life's path through personalized readings and horoscopes.",
+          para2:
+            "From understanding your past to navigating your future, our astrology services help you achieve clarity and confidence.",
+        },
+        section2: {
+          heading: "Meet the Astrologer",
+          img: "https://i.pinimg.com/236x/06/0e/fa/060efac4c4791fbf60aaeced4aed9c36.jpg", // Replace with actual image URL
+          para1:
+            "With years of experience, our astrologer brings wisdom and compassion to every consultation.",
+          para2: [
+            "Specializes in natal charts, compatibility analysis, and career guidance",
+            "Offers personalized readings tailored to your unique circumstances",
+            "Combines traditional and modern astrology techniques",
+          ],
+        },
+        section3: {
+          heading: "Guidance Aligned with the Stars",
+          img: "https://i.pinimg.com/736x/1c/d6/44/1cd6445d62129261fe472682c6bf9b27.jpg", // Replace with actual image URL
+          para1:
+            "Our astrologers continuously refine their expertise to provide accurate and insightful readings.",
+          para2:
+            "Each session is designed to empower you to make informed decisions with celestial guidance.",
+        },
+        testimonials: [
+          { quote: "The astrology reading helped me find direction in life.", name: "Anna B." },
+          { quote: "I gained a deeper understanding of myself. Thank you!", name: "Michael T." },
+          { quote: "Highly accurate and insightful. I recommend their services.", name: "Sophia L." },
+          { quote: "Their guidance gave me clarity on important life decisions.", name: "Emma K." },
+        ],
+      },
+      {
+        name: "Tarot card reader",
+        link: "/categories/makeup-artist",
+        heroSection: {
+          text: "Find Clarity with Expert Tarot Card Readings",
+          img: "https://i.pinimg.com/236x/9b/ef/b7/9befb7857dadd35d9ec95b9a97d0b949.jpg", // Replace with actual image URL
+        },
+        section1: {
+          heading: "Unlock the Mysteries of Life",
+          img: "https://i.pinimg.com/236x/6c/d9/2c/6cd92ce5b351c824b4e7e123b7f50002.jpg", // Replace with actual image URL
+          para1:
+            "Our tarot card readers provide deep insights and answers to your most pressing questions.",
+          para2:
+            "Experience the power of tarot as a tool for guidance, clarity, and self-discovery.",
+        },
+        section2: {
+          heading: "Meet the Tarot Reader",
+          img: "https://i.pinimg.com/236x/47/51/b8/4751b81bb584c0f0451ffdcd35d2e62e.jpg", // Replace with actual image URL
+          para1:
+            "Our tarot reader combines intuitive skills with years of experience to offer meaningful readings.",
+          para2: [
+            "Specializes in love, career, and spiritual guidance",
+            "Provides personalized spreads to address your concerns",
+            "Creates a calming and supportive environment",
+          ],
+        },
+        section3: {
+          heading: "Unveil the Path Ahead",
+          img: "https://i.pinimg.com/236x/f2/23/64/f223641855641754097ff35db4fcd3a7.jpg", // Replace with actual image URL
+          para1:
+            "Our tarot card reader keeps learning new techniques to ensure you receive accurate and insightful guidance.",
+          para2:
+            "Each reading is tailored to empower and inspire you on your journey.",
+        },
+        testimonials: [
+          { quote: "The tarot reading was spot on! It gave me clarity.", name: "Daniel R." },
+          { quote: "I felt so much lighter after the session. Amazing experience!", name: "Laura P." },
+          { quote: "The reader’s insights were incredibly accurate.", name: "Tom H." },
+          { quote: "Highly recommend for anyone seeking guidance.", name: "Emma W." },
+        ],
+      },
+      { name: "Makeup artist", link: "/categories/makeup-artist",
+        heroSection: { text: "Enhance Your Beauty with Expert Makeup Artistry", img: image1 },
+        section1: { heading: "Elevate Your Style",img:image2 , para1: "Embrace the transformation! Our expert makeup artists specialize in bringing out your inner beauty, whether it’s for an unforgettable wedding day, a captivating photoshoot, or a special celebration. Let us help you look and feel your best!",para2:"Experience a personalized touch with makeup designed to suit your unique features. We offer a range of services tailored to make you feel radiant and confident for any event." },
+        section2:{heading:"Meet the Artist",img:image3,para1:"Our makeup artist is a skilled professional with years of experience creating stunning looks for all occasions. From bridal makeup to editorial shoots, their artistry enhances natural beauty with a touch of glamour.",para2:["Expert in bridal makeup, photoshoots, and special events","Uses premium quality products for long-lasting results","Provides personalized makeup consultations"]},
+        section3:{heading:"Always Innovating, Always Improving", img:image4, para1:"We are always on the lookout for the latest trends, products, and techniques to ensure our clients receive the best possible service.", para2:"Our dedication to innovation means we are constantly improving, ensuring your experience with us is fresh, exciting, and always top-notch."},
+        testimonials:[
+          {
+            quote: "The makeup artist transformed my look for my wedding day! I felt beautiful and confident. The makeup stayed on all day, and I received so many compliments!",
+            name: "Sarah J.",
+          },
+          {
+            quote: "I had my first photoshoot and the makeup artist made me feel like a star. The look was perfect for the camera, and I loved how my makeup turned out!",
+            name: "Emily R.",
+          },
+          {
+            quote: "She made my prom night unforgettable! The makeup was flawless and enhanced my features so naturally. I received so many compliments throughout the evening!",
+            name: "Olivia W.",
+          },
+          {
+            quote: "The makeup artist really understood my skin tone and gave me a fresh, natural look that lasted for hours! I will definitely book again for my next event.",
+            name: "Rachel T.",
+          },
+        ]
+       },
+      {
+        name: "Beautician",
+        link: "/categories/makeup-artist",
+        heroSection: {
+          text: "Enhance Your Natural Beauty with Expert Care",
+          img: "beautician_hero_image.jpg", // Replace with actual image URL
+        },
+        section1: {
+          heading: "Pamper Yourself with Professional Services",
+          img: "beautician_section1_image.jpg", // Replace with actual image URL
+          para1:
+            "Our beauticians provide a wide range of services to enhance your natural glow and keep your skin healthy.",
+          para2:
+            "From facials to waxing, our treatments are designed to rejuvenate and refresh.",
+        },
+        section2: {
+          heading: "Meet the Beautician",
+          img: "beautician_section2_image.jpg", // Replace with actual image URL
+          para1:
+            "Our experienced beauticians are dedicated to providing top-notch services with a personalized touch.",
+          para2: [
+            "Specializes in skincare, makeup, and grooming services",
+            "Uses high-quality, skin-safe products",
+            "Offers consultations to meet your beauty goals",
+          ],
+        },
+        section3: {
+          heading: "Innovative Techniques for Timeless Beauty",
+          img: "beautician_section3_image.jpg", // Replace with actual image URL
+          para1:
+            "Our beauticians are always updating their skills to provide the latest and best techniques.",
+          para2:
+            "Experience the perfect blend of relaxation and beauty enhancement in every session.",
+        },
+        testimonials: [
+          { quote: "My skin feels amazing after the facial!", name: "Lily A." },
+          { quote: "Their services are top-notch. I felt so pampered.", name: "Nina R." },
+          { quote: "The beautician was very professional and skilled.", name: "James M." },
+          { quote: "I’ll definitely come back for more treatments.", name: "Sophia L." },
+        ],
+      },
+      {
+        name: "Mehendi artist",
+        link: "/categories/makeup-artist",
+        heroSection: {
+          text: "Celebrate Traditions with Beautiful Mehendi Designs",
+          img: "mehendi_artist_hero_image.jpg", // Replace with actual image URL
+        },
+        section1: {
+          heading: "Intricate Designs for Special Occasions",
+          img: "mehendi_artist_section1_image.jpg", // Replace with actual image URL
+          para1:
+            "Our mehendi artists craft intricate and stunning designs that add elegance to every occasion.",
+          para2:
+            "From bridal mehendi to festive celebrations, we ensure every design is unique and beautiful.",
+        },
+        section2: {
+          heading: "Meet the Mehendi Artist",
+          img: "mehendi_artist_section2_image.jpg", // Replace with actual image URL
+          para1:
+            "Our mehendi artist is skilled in traditional and modern styles, creating designs to suit your taste.",
+          para2: [
+            "Expert in bridal, festive, and personalized designs",
+            "Uses organic and skin-safe mehendi",
+            "Offers quick and detailed application services",
+          ],
+        },
+        section3: {
+          heading: "Tradition Meets Creativity",
+          img: "mehendi_artist_section3_image.jpg", // Replace with actual image URL
+          para1:
+            "Our artists continuously refine their techniques to ensure every design is a masterpiece.",
+          para2:
+            "Celebrate your special moments with the perfect blend of tradition and creativity.",
+        },
+        testimonials: [
+          { quote: "My bridal mehendi was stunning! Thank you!", name: "Priya S." },
+          { quote: "The designs were so intricate and beautiful.", name: "Rina P." },
+          { quote: "Highly professional and talented artist.", name: "Anjali T." },
+          { quote: "I loved the mehendi! It made my event even more special.", name: "Neha M." },
+        ],
+      },
+      {
+        name: "Tattoo artist",
+        link: "/categories/makeup-artist",
+        heroSection: {
+          text: "Express Yourself with Stunning Tattoo Art",
+          img: "tattoo_artist_hero_image.jpg",
+        },
+        section1: {
+          heading: "Create Your Unique Story",
+          img: "tattoo_artist_section1_image.jpg",
+          para1:
+            "Our talented tattoo artists bring your vision to life with custom designs that reflect your individuality.",
+          para2:
+            "From intricate details to bold statements, we ensure every tattoo is a work of art tailored to you.",
+        },
+        section2: {
+          heading: "Meet the Tattoo Artist",
+          img: "tattoo_artist_section2_image.jpg",
+          para1:
+            "Our tattoo artist combines creativity and precision to deliver exceptional tattooing experiences.",
+          para2: [
+            "Expert in traditional, tribal, and modern tattoo styles",
+            "Uses high-quality, sterile equipment for safety and comfort",
+            "Provides detailed consultations to understand your vision",
+          ],
+        },
+        section3: {
+          heading: "Art that Evolves with You",
+          img: "tattoo_artist_section3_image.jpg",
+          para1:
+            "Our artists continuously refine their skills and stay updated with the latest trends and techniques.",
+          para2:
+            "Every tattoo is a unique creation designed to be meaningful and timeless.",
+        },
+        testimonials: [
+          { quote: "The artist designed my dream tattoo perfectly!", name: "Mike T." },
+          { quote: "I felt at ease throughout the process. Amazing work!", name: "Jessica F." },
+          { quote: "My tattoo is a masterpiece I’ll cherish forever.", name: "Daniel G." },
+          { quote: "Their creativity and professionalism are unmatched.", name: "Anna L." },
+        ],
+      },
+      {
+        name: "Hair salons",
+        link: "/categories/makeup-artist",
+        heroSection: {
+          text: "Transform Your Look with Expert Hair Care",
+          img: "hair_salon_hero_image.jpg",
+        },
+        section1: {
+          heading: "Discover Your Signature Style",
+          img: "hair_salon_section1_image.jpg",
+          para1:
+            "Our hair salons offer a wide range of services to refresh your look and enhance your confidence.",
+          para2:
+            "From trendy cuts to advanced treatments, experience personalized hair care tailored to your unique style.",
+        },
+        section2: {
+          heading: "Meet the Stylists",
+          img: "hair_salon_section2_image.jpg",
+          para1:
+            "Our team of expert stylists brings years of experience and a passion for creativity to every client.",
+          para2: [
+            "Specializes in haircuts, coloring, and styling",
+            "Uses premium hair care products for lasting results",
+            "Provides consultations to create your ideal look",
+          ],
+        },
+        section3: {
+          heading: "Innovative Techniques for Stunning Results",
+          img: "hair_salon_section3_image.jpg",
+          para1:
+            "We stay updated with the latest hair trends and techniques to ensure you always leave our salon looking fabulous.",
+          para2:
+            "From classic styles to cutting-edge looks, our dedication to excellence shines through in every service.",
+        },
+        testimonials: [
+          { quote: "I love my new haircut! The stylist was amazing.", name: "Sophie K." },
+          { quote: "Their coloring services are top-notch. Highly recommend!", name: "Ryan M." },
+          { quote: "The staff is friendly and professional. Great experience!", name: "Emma T." },
+          { quote: "My hair feels healthier and looks stunning after the treatment.", name: "Lily C." },
+        ],
+      },
     ],
   },
   {
@@ -205,7 +472,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="relative z-10 shadow-md w-full bg-white text-black">
+    <div className="relative z-30 shadow-md w-full bg-white text-black">
       <div className="container mx-auto py-2 px-4">
         <div className="flex justify-between items-center">
           {/* Logo */}
@@ -222,11 +489,11 @@ const Navbar = () => {
                   key={id}
                   className="relative group"
                   onMouseEnter={() => setDropdown(id)}
-                  onMouseLeave={() => setDropdown(null)}
+                  // onMouseLeave={() => setDropdown(null)}
                 >
                   <a
                     onClick={() => setDropdown(id)}
-                    href={link || "#"}
+                    href={link}
                     className="text-lg font-medium flex items-center gap-2 text-black hover:text-primary transition-colors duration-300"
                   >
                     {name}
--- src/pages/Categories/MakeupArtist.jsx
+++ src/pages/Categories/MakeupArtist.jsx
@@ -3,30 +3,35 @@ import image1 from "../../assets/image7.jpg";
 import image2 from "../../assets/bride.jpg";
 import image3 from "../../assets/makeup1.jpg";
 import image4 from "../../assets/image5.jpg";
-
+import { useLocation } from 'react-router-dom';
+import { li } from 'framer-motion/client';
 // Testimonial data
-const testimonials = [
-  {
-    quote: "The makeup artist transformed my look for my wedding day! I felt beautiful and confident. The makeup stayed on all day, and I received so many compliments!",
-    name: "Sarah J.",
-  },
-  {
-    quote: "I had my first photoshoot and the makeup artist made me feel like a star. The look was perfect for the camera, and I loved how my makeup turned out!",
-    name: "Emily R.",
-  },
-  {
-    quote: "She made my prom night unforgettable! The makeup was flawless and enhanced my features so naturally. I received so many compliments throughout the evening!",
-    name: "Olivia W.",
-  },
-  {
-    quote: "The makeup artist really understood my skin tone and gave me a fresh, natural look that lasted for hours! I will definitely book again for my next event.",
-    name: "Rachel T.",
-  },
-];
+// const testimonials = [
+//   {
+//     quote: "The makeup artist transformed my look for my wedding day! I felt beautiful and confident. The makeup stayed on all day, and I received so many compliments!",
+//     name: "Sarah J.",
+//   },
+//   {
+//     quote: "I had my first photoshoot and the makeup artist made me feel like a star. The look was perfect for the camera, and I loved how my makeup turned out!",
+//     name: "Emily R.",
+//   },
+//   {
+//     quote: "She made my prom night unforgettable! The makeup was flawless and enhanced my features so naturally. I received so many compliments throughout the evening!",
+//     name: "Olivia W.",
+//   },
+//   {
+//     quote: "The makeup artist really understood my skin tone and gave me a fresh, natural look that lasted for hours! I will definitely book again for my next event.",
+//     name: "Rachel T.",
+//   },
+// ];
 
 const MakeupArtist = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-
+  const location = useLocation()
+  const category = location.state
+  console.log("category")
+  console.log(category)
+  const testimonials = category.testimonials
   // Auto slide every 3 seconds
   useEffect(() => {
     const interval = setInterval(() => {
@@ -44,10 +49,10 @@ const MakeupArtist = () => {
     <div className="mx-auto my-2">
 
       {/* Hero Section */}
-      <div className="relative bg-cover bg-center h-[350px] sm:h-[400px] lg:h-[600px] w-full rounded-xl mb-6 sm:mb-8 lg:mb-12" style={{ backgroundImage: `url(${image1})` }}>
+      <div className="relative bg-cover bg-center h-[350px] sm:h-[400px] lg:h-[600px] w-full rounded-xl mb-6 sm:mb-8 lg:mb-12" style={{ backgroundImage: `url(${category.heroSection.img})` }}>
       <div className="absolute inset-0 bg-black opacity-50 rounded-xl"></div>
         <div className="relative z-10 text-center text-white py-10 sm:py-16 lg:py-24 px-6 sm:px-8 lg:px-12">
-          <h1 className="text-2xl sm:text-3xl lg:text-5xl font-extrabold mb-6">Enhance Your Beauty with Expert Makeup Artistry</h1>
+          <h1 className="text-2xl sm:text-3xl lg:text-5xl font-extrabold mb-6">{category.heroSection.text}</h1>
           <p className="text-sm sm:text-lg lg:text-xl mb-8 sm:mb-10">Bridal, Photoshoots, Events - We make every occasion unforgettable.</p>
           <a href="#contact" className="bg-primary text-white py-2 px-6 sm:px-8 rounded-full text-sm sm:text-lg font-semibold hover:bg-primary-dark transition-all">
             Book Your Session
@@ -59,18 +64,18 @@ const MakeupArtist = () => {
       <div className="flex flex-col lg:flex-row items-center lg:justify-between mb-6 sm:mb-8 lg:mb-12 bg-gradient-to-r from-gray-100 to-gray-300 rounded-xl shadow-xl" id="transform">
   <div className="text-left lg:w-1/2 p-4 sm:p-8">
     <h2 className="text-3xl sm:text-4xl lg:text-5xl font-extrabold text-gray-800 mb-6 leading-snug text-shadow-md">
-      Elevate Your Style
+      {category.section1.heading}
     </h2>
     <p className="text-base sm:text-lg lg:text-xl text-gray-700 leading-relaxed mb-6">
-      Embrace the transformation! Our expert makeup artists specialize in bringing out your inner beauty, whether it’s for an unforgettable wedding day, a captivating photoshoot, or a special celebration. Let us help you look and feel your best!
+      {category.section1.para1}
     </p>
     <p className="text-base sm:text-lg lg:text-xl text-gray-700 leading-relaxed mb-6">
-      Experience a personalized touch with makeup designed to suit your unique features. We offer a range of services tailored to make you feel radiant and confident for any event.
+    {category.section1.para2}
     </p>
   </div>
   <div className="lg:w-1/2 p-4 sm:p-10">
     <img 
-      src={image2} 
+      src={category.section1.img} 
       alt="Transform Your Look"
       className="w-[600px] h-[250px] sm:h-[350px] lg:h-[400px] object-cover rounded-2xl shadow-2xl transform hover:scale-105 transition-transform duration-300 ease-in-out"
     />
@@ -81,19 +86,19 @@ const MakeupArtist = () => {
       {/* Additional Sections 2*/}
       <div className="flex flex-col lg:flex-row-reverse items-center lg:justify-between mb-6 sm:mb-8 lg:mb-14">
         <div className="text-left lg:w-1/2 p-4 sm:p-6">
-          <h2 className="text-3xl sm:text-4xl lg:text-5xl font-extrabold text-gray-800 mb-6 leading-snug">Meet the Artist</h2>
+          <h2 className="text-3xl sm:text-4xl lg:text-5xl font-extrabold text-gray-800 mb-6 leading-snug">{category.section2.heading}</h2>
           <p className="text-base sm:text-lg lg:text-xl text-gray-700 leading-relaxed mb-4">
-            Our makeup artist is a skilled professional with years of experience creating stunning looks for all occasions. From bridal makeup to editorial shoots, their artistry enhances natural beauty with a touch of glamour.
+          {category.section2.para1}
           </p>
           <ul className="list-disc pl-6 text-lg text-gray-600">
-            <li>Expert in bridal makeup, photoshoots, and special events</li>
-            <li>Uses premium quality products for long-lasting results</li>
-            <li>Provides personalized makeup consultations</li>
+          {category.section2.para2.map((item,index)=>(
+            <li key={index}>{item}</li>
+          ))}
           </ul>
         </div>
         <div className="lg:w-1/2 p-4 sm:p-6">
           <img 
-            src={image3} 
+            src={category.section2.img}
             alt="Makeup Artist" 
             className="w-[600px] h-[250px] sm:h-[300px] lg:h-[400px] object-cover rounded-2xl shadow-2xl transform hover:scale-105 transition-transform duration-300 ease-in-out"
           />
@@ -104,18 +109,18 @@ const MakeupArtist = () => {
       <div className="relative flex flex-col lg:flex-row items-center lg:justify-between mb-6 sm:mb-8 lg:mb-14 bg-gradient-to-r from-gray-100 to-gray-300 rounded-xl shadow-xl"  id="our-story">
         <div className="text-left lg:w-1/2 p-4 sm:p-6">
           <h2 className="text-3xl sm:text-4xl lg:text-5xl font-extrabold text-gray-900 mb-6 leading-snug">
-            Always Innovating, Always Improving
+          {category.section3.heading}
           </h2>
           <p className="text-base sm:text-lg lg:text-xl text-gray-600 leading-relaxed mb-6">
-            We are always on the lookout for the latest trends, products, and techniques to ensure our clients receive the best possible service.
+          {category.section3.para1}
           </p>
           <p className="text-base sm:text-lg lg:text-xl text-gray-600 leading-relaxed mb-6">
-            Our dedication to innovation means we are constantly improving, ensuring your experience with us is fresh, exciting, and always top-notch.
+          {category.section3.para2}
           </p>
         </div>
         <div className="lg:w-1/2 p-4 sm:p-6 relative z-20">
           <img
-            src={image4}
+            src={category.section3.img}
             alt="Innovative Beauty"
             className="w-[600px] h-[250px] sm:h-[350px] lg:h-[400px] object-cover rounded-2xl shadow-xl transform hover:scale-110 transition-transform duration-500 ease-in-out"
           />
